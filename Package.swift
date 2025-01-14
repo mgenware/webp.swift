7d8484143d47cf0fd0b66330769b9270fecbd9b3
Liu YuanYuan <m@mgenware.com>
2025-01-14 19:09:33 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["webp", "webpbridge"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/awxkee/libwebp-ios.git", "1.1.0"..<"1.2.0")
+        .package(url: "https://github.com/awxkee/libwebp-ios.git", "1.2.0"..<"1.3.0")
     ],
     targets: [
         .target(
